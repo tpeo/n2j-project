0ed71ded4ce013477f8b0a7ebfd5557cc392a9f7
SandyCreek <xiezhuoli771@gmail.com>
2023-02-08 16:19:52 -0600

    frontend form HTML completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,28 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <br></br>
+      <label>
+        Distance to campus in meters:
+        <input type="number" name = "distance" />
+      </label>
+      <br></br>
+      <label>
+        Star rating:
+        1 <input type="radio" id="star1" name="star" value="1"></input>
+        2 <input type="radio" id="star2" name="star" value="2"></input>  
+        3 <input type="radio" id="star3" name="star" value="3"></input>  
+        4 <input type="radio" id="star4" name="star" value="4"></input>  
+        5 <input type="radio" id="star5" name="star" value="5"></input>  
+      </label>
+      <br></br>
+      <input type="submit" value="Submit" />
+    </form>
   </React.StrictMode>
 );
 
