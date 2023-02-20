65bb894373b38d1ee2aed094d7a2d741ba71b0c5
SandyCreek <xiezhuoli771@gmail.com>
2023-02-20 17:58:44 -0600

    fixed add-apt, added get-apt

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,14 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.post('/add-entry', async (req, res) => {
+app.post('/add-apt', async (req, res) => {
   try {
     console.log(req.body)
     const { name, apt_id } = req.body; // Assuming request body contains 'name' and 'description' fields
     
     // Add new entry to Firestore collection
-    const entryRef = await firebase.db.collection('houses').doc("wntun3OGZXkvB6GMSKKZ").set({
+    // Apartment ID will be used to identify it in the database
+    const entryRef = await firebase.db.collection('houses').doc(apt_id + "").set({
       name,
       apt_id
     });
@@ -22,6 +23,26 @@ app.post('/add-entry', async (req, res) => {
     res.status(500).send({ error: 'Error adding entry' });
   }
 });
+
+app.get('/get-apt', async (req, res) => {
+  try {
+    console.log(req.body);
+    const {apt_id} = req.body;
+    const entryRef = await firebase.db.collection('houses').doc(apt_id+"");
+    const doc = await entryRef.get();
+    if (doc.exists) {
+      res.status(200).send(doc.data());
+    }
+    else {
+      res.status(200).send("No such apartment");
+    }
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).send("Can't retrieve this apartment");
+  }
+})
+
 app.listen(4000, () => {
   console.log("Server running on port 4000");
 });
