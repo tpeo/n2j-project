199a9c5f4138dbcc1de75b79465102b4d73533f6
Nayeon Kim <nayeonkim@Nayeons-MacBook-Air.local>
2023-02-26 16:35:52 -0600

    edits to search bar

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -4,7 +4,7 @@ const SearchBar = ({ placeholder, data }) => {
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" />
+        <input type="text" placeholder={placeholder} />
         <div className="searchIcon"></div>
       </div>
       <div className="dataResults"></div>
