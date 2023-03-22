3ac697125f64d91bcd1d0de5b844491ac64507c7
SandyCreek <xiezhuoli771@gmail.com>
2023-03-21 23:05:27 -0500

    removed logout page

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -1,8 +1,31 @@
 import {Container, Nav, Navbar} from 'react-bootstrap';
+import React, { useEffect, useState, useContext } from "react";
+import AuthContext from "../context/authentication.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Layout = () => {
+  const [error, setError] = useState("");
+
+  const auth = useContext(AuthContext);
+
+  console.log(auth);
+
+  useEffect(() => {
+      if (error) {
+          window.alert(error);
+      }
+  }, [error]);
+
+  const handleLogout = async (event) => {
+      event.preventDefault();
+      if (auth.loggedIn) {
+          auth.logout();
+      } 
+      else {
+          setError("Not logged in ---- button should not work.");
+      }
+  };
   const email = window.localStorage.getItem("username");
 
   return (
@@ -14,7 +37,7 @@ const Layout = () => {
             <Nav.Link href="/apts">All Apartments</Nav.Link>
             {email ? <Nav.Link href="/myapts">My Apartments</Nav.Link> : <p></p>}
             <Nav.Link href="/newapt" disabled>Submit New Apartment</Nav.Link>
-            {email ? <Nav.Link href="/logout">Logout</Nav.Link> : <Nav.Link href="/login">Login</Nav.Link>}
+            {email ? <Nav.Link onClick={handleLogout} href="/apts">Logout</Nav.Link> : <Nav.Link href="/login">Login</Nav.Link>}
             {email ? <p></p> : <Nav.Link href="/signup">Signup</Nav.Link>}
           </Nav>
         </Container>
