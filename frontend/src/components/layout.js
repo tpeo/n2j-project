3b0fd2fe776107aa549fcd402604f3cb1acecddf
Nayeon Kim <knayeon117@gmail.com>
2023-04-10 03:48:51 -0500

    made navbar none transparent

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -9,7 +9,7 @@ import {
   SignLanguage,
   Menu,
   Close,
-  AccountCircle
+  AccountCircle,
 } from "@mui/icons-material";
 
 const Layout = () => {
@@ -18,13 +18,13 @@ const Layout = () => {
   const auth = useContext(AuthContext);
 
   const [sidebar, setSidebar] = useState(false);
-  const [navbar, setNavbar] = useState('--navbar-close');
+  const [navbar, setNavbar] = useState("--navbar-close");
 
   useEffect(() => {
     if (sidebar) {
-      setNavbar('--navbar-close');
+      setNavbar("--navbar-close");
     } else {
-      setNavbar('--navbar-open');
+      setNavbar("--navbar-open");
     }
     console.log(navbar);
   }, [sidebar]);
@@ -36,19 +36,18 @@ const Layout = () => {
   console.log(auth);
 
   useEffect(() => {
-      if (error) {
-          window.alert(error);
-      }
+    if (error) {
+      window.alert(error);
+    }
   }, [error]);
 
   const handleLogout = async (event) => {
-      event.preventDefault();
-      if (auth.loggedIn) {
-          auth.logout();
-      } 
-      else {
-          setError("Not logged in ---- button should not work.");
-      }
+    event.preventDefault();
+    if (auth.loggedIn) {
+      auth.logout();
+    } else {
+      setError("Not logged in ---- button should not work.");
+    }
   };
   const email = window.localStorage.getItem("username");
 
@@ -318,10 +317,64 @@ const Layout = () => {
       </nav>
     </div>
     */
-       <>
-      <div className="navbar" style={{ '--navbar': `var(${navbar})` }}>
+    <>
+      <div className="navbar" style={{ "--navbar": `var(${navbar})` }}>
         <a className="menu-bars">
-          <Menu onClick={showSidebar} />
+          {/* <Menu onClick={showSidebar} /> */}
+          <div onClick={showSidebar}>
+            <svg
+              width="49"
+              height="62"
+              viewBox="0 0 49 62"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_835_2343)">
+                <path
+                  d="M24.1707 1.93817H24.1642C11.8892 1.93817 1.93829 11.8891 1.93829 24.1641V37.8356C1.93829 50.1107 11.8892 60.0616 24.1642 60.0616H24.1707C36.4457 60.0616 46.3966 50.1107 46.3966 37.8356V24.1641C46.3966 11.8891 36.4457 1.93817 24.1707 1.93817Z"
+                  stroke="#88B4D9"
+                  stroke-width="3.87662"
+                  stroke-miterlimit="10"
+                />
+                <path
+                  d="M24.1707 1.93817H24.1642C11.8892 1.93817 1.93829 11.8891 1.93829 24.1641V37.8356C1.93829 50.1107 11.8892 60.0616 24.1642 60.0616H24.1707C36.4457 60.0616 46.3966 50.1107 46.3966 37.8356V24.1641C46.3966 11.8891 36.4457 1.93817 24.1707 1.93817Z"
+                  stroke="#5598CF"
+                  stroke-opacity="0.19"
+                  stroke-width="3.87662"
+                  stroke-miterlimit="10"
+                />
+                <path
+                  d="M12.0886 26.7358L9.0325 29.0101L11.436 31.8077C11.5071 37.4934 11.5006 42.1647 11.4683 45.8023C11.4683 46.2998 11.4683 46.3838 11.4683 46.5647C11.688 51.7981 16.0556 57.5613 21.6961 59.1314C27.3624 60.7079 32.9512 57.6647 36.7761 55.5972C37.9327 54.9769 39.535 53.9432 41.0663 52.2698C42.1388 51.1003 42.8754 49.9438 43.3793 49.0069C36.5629 36.6276 31.0129 27.4142 28.2928 23.4084C27.5046 22.2518 26.0056 20.0809 23.3437 19.241C21.1534 18.5497 19.2668 18.9632 18.485 19.1893C17.6257 19.493 16.0427 20.1778 14.6084 21.7027C12.7541 23.6603 12.2372 25.8442 12.0756 26.7487L12.0886 26.7358Z"
+                  fill="#FFFDF9"
+                  fill-opacity="0.34"
+                />
+                <path
+                  d="M12.0886 26.7358L9.0325 29.0101L11.436 31.8077C11.5071 37.4934 11.5006 42.1647 11.4683 45.8023C11.4683 46.2998 11.4683 46.3838 11.4683 46.5647C11.688 51.7981 16.0556 57.5613 21.6961 59.1314C27.3624 60.7079 32.9512 57.6647 36.7761 55.5972C37.9327 54.9769 39.535 53.9432 41.0663 52.2698C42.1388 51.1003 42.8754 49.9438 43.3793 49.0069C36.5629 36.6276 31.0129 27.4142 28.2928 23.4084C27.5046 22.2518 26.0056 20.0809 23.3437 19.241C21.1534 18.5497 19.2668 18.9632 18.485 19.1893C17.6257 19.493 16.0427 20.1778 14.6084 21.7027C12.7541 23.6603 12.2372 25.8442 12.0756 26.7487L12.0886 26.7358Z"
+                  stroke="#88B4D9"
+                  stroke-width="3.87662"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M12.0886 26.7358L9.0325 29.0101L11.436 31.8077C11.5071 37.4934 11.5006 42.1647 11.4683 45.8023C11.4683 46.2998 11.4683 46.3838 11.4683 46.5647C11.688 51.7981 16.0556 57.5613 21.6961 59.1314C27.3624 60.7079 32.9512 57.6647 36.7761 55.5972C37.9327 54.9769 39.535 53.9432 41.0663 52.2698C42.1388 51.1003 42.8754 49.9438 43.3793 49.0069C36.5629 36.6276 31.0129 27.4142 28.2928 23.4084C27.5046 22.2518 26.0056 20.0809 23.3437 19.241C21.1534 18.5497 19.2668 18.9632 18.485 19.1893C17.6257 19.493 16.0427 20.1778 14.6084 21.7027C12.7541 23.6603 12.2372 25.8442 12.0756 26.7487L12.0886 26.7358Z"
+                  stroke="#5598CF"
+                  stroke-opacity="0.19"
+                  stroke-width="3.87662"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M20.4173 30.2388C21.3236 30.2388 22.0584 29.5041 22.0584 28.5977C22.0584 27.6914 21.3236 26.9566 20.4173 26.9566C19.5109 26.9566 18.7762 27.6914 18.7762 28.5977C18.7762 29.5041 19.5109 30.2388 20.4173 30.2388Z"
+                  fill="#88B4D9"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_835_2343">
+                  <rect width="48.3349" height="62" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </div>
         </a>
       </div>
       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
--- frontend/src/pages/apts.js
+++ frontend/src/pages/apts.js
@@ -3,7 +3,7 @@ import { Button, Card, Row, Col, Form } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import "./apts.css";
-import { IconButton, TextField } from "@mui/material";
+import { IconButton, TextField, Box } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 const Apts = () => {
@@ -53,78 +53,97 @@ const Apts = () => {
 
   return (
     <div class="aptlist">
-      <h1>Home</h1>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Label class="content">Search for Apartments</Form.Label>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Form.Control
-              onChange={(event) => setName(event.target.value)}
-              placeholder="Enter name"
-            />{" "}
-          </Col>
-          <Col>
-            <Button
-              variant="primary"
-              type="button"
-              onClick={() => setButton(!button)}
-            >
-              Search
-            </Button>
-          </Col>
-        </Row>
-      </Form>
-      <div class="aptcards">
-        <Row xs={1} md={2} className="g-4">
-          {apts &&
-            apts.map((apt) => (
-              <div>
-                <br />
+      <Row className="g-0">
+        <Col md={6} className="g-0">
+          <div class="left">
+            <h1>Home</h1>
+            <Form>
+              <Row>
                 <Col>
-                  <a href={"/apts/" + apt["apt_id"]}>
-                    <Card class="entirecard">
-                      <Card.Body>
-                        <Card.Img
-                          variant="top"
-                          src={apt["aptimage"]}
-                          class="images"
-                        />
-                        <br />
-                        <Card.Title class="aptname">{apt["name"]}</Card.Title>
-                        <Card.Text class="content">
-                          Apartment ID: {apt["apt_id"]}
-                          <br />
-                          Rating:{" "}
-                          {apt["reviews"] &&
-                            parseFloat(
-                              apt["reviews"].reduce(
-                                (v, review) =>
-                                  (v =
-                                    v +
-                                    parseInt(review["cleanliness"], 10) +
-                                    parseInt(review["maintenance"], 10) +
-                                    parseInt(review["amenities"], 10) +
-                                    parseInt(review["conditions"], 10)),
-                                0
-                              ) / (4 * apt["reviews"].length).toFixed(1)
-                            )}
-                          <br />
-                          {apt["address"]}
-                          <br />
-                        </Card.Text>
-                        {/* <Button href={"/apts/" + apt["apt_id"]}>See More</Button> */}
-                      </Card.Body>
-                    </Card>
-                  </a>
+                  <Form.Label class="content">Search for Apartments</Form.Label>
                 </Col>
-              </div>
-            ))}
-        </Row>
-      </div>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Control
+                    onChange={(event) => setName(event.target.value)}
+                    placeholder="Enter name"
+                  />{" "}
+                </Col>
+                <Col>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    onClick={() => setButton(!button)}
+                  >
+                    Search
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+            <div class="aptcards">
+              <Row xs={1} md={2} className="g-4">
+                {apts &&
+                  apts.map((apt) => (
+                    <div>
+                      <br />
+                      <Col>
+                        <a href={"/apts/" + apt["apt_id"]}>
+                          <Card class="entirecard">
+                            <Card.Body>
+                              <Card.Img
+                                variant="top"
+                                src={apt["aptimage"]}
+                                class="images"
+                              />
+                              <br />
+                              <Card.Title class="aptname">
+                                {apt["name"]}
+                              </Card.Title>
+                              <Card.Text class="content">
+                                Apartment ID: {apt["apt_id"]}
+                                <br />
+                                Rating:{" "}
+                                {apt["reviews"] &&
+                                  parseFloat(
+                                    apt["reviews"].reduce(
+                                      (v, review) =>
+                                        (v =
+                                          v +
+                                          parseInt(review["cleanliness"], 10) +
+                                          parseInt(review["maintenance"], 10) +
+                                          parseInt(review["amenities"], 10) +
+                                          parseInt(review["conditions"], 10)),
+                                      0
+                                    ) / (4 * apt["reviews"].length).toFixed(1)
+                                  )}
+                                <br />
+                                {apt["address"]}
+                                <br />
+                              </Card.Text>
+                              {/* <Button href={"/apts/" + apt["apt_id"]}>See More</Button> */}
+                            </Card.Body>
+                          </Card>
+                        </a>
+                      </Col>
+                    </div>
+                  ))}
+              </Row>
+            </div>
+          </div>
+        </Col>
+        <Col md={6} className="g-0">
+          <div class="right">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3445.3209627205215!2d-97.73624538482487!3d30.2849231141269!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b59b2584cfb7%3A0x8131ee4f174a21de!2sThe%20University%20of%20Texas%20at%20Austin!5e0!3m2!1sen!2sus!4v1681102840582!5m2!1sen!2sus"
+              style={{ border: "0" }}
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
