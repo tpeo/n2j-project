6f5ef36c26a05fe5c308a59c9d08a38aa594b263
Nayeon Kim <knayeon117@gmail.com>
2023-04-10 08:30:25 -0500

    polished login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/authentication.js";
-import { Button, TextField } from '@mui/material';
+import {
+  Button,
+  TextField,
+  Card,
+  CardContent,
+  Typography,
+  Avatar,
+} from "@mui/material";
+import { CelebrationTwoTone } from "@mui/icons-material";
+import "./login.css";
 //import TextField from '@mui/material/TextField';
-
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -51,27 +59,89 @@ function Login() {
     }
   };
 
-    const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value,
-        });
-    };
-    
-    return (
-        <div class="aptlist">
-            <form>
-                <h1>Email: </h1> 
-                <TextField id="email" label="Email" variant="outlined" type="text" name="email" onChange={handleChange} />
-                <h1>Password:</h1>
-                <TextField id="password" label="Password" variant="outlined" type="password" name="password" onChange={handleChange} />
-                <br />
-                <Button variant="contained" onClick={handleLogin}>Login</Button>
-            </form>
-        </div>
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    );
-
+  return (
+    <div class="aptlist">
+      <form class="card">
+        <Card sx={{ maxWidth: 345 }} class="inner">
+          <CardContent>
+            <div class="right">
+              <div class="user">
+                <Avatar
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    background: "#5ea0d6",
+                  }}
+                >
+                  <CelebrationTwoTone
+                    style={{ width: "60px", height: "60px" }}
+                  />
+                </Avatar>
+              </div>
+              <div class="loginletter">
+                <Typography variant="h3" component="div">
+                  Login
+                </Typography>
+              </div>
+              <div class="caption">
+                <Typography>
+                  Start your journey with us! We are happy to welcome you back.
+                  Noot is a housing app developed by ut students aiming to make
+                  finding housing easier for current/prospective ut students.
+                </Typography>
+              </div>
+              <div class="loginelement">
+                <TextField
+                  id="email"
+                  label="Email"
+                  variant="outlined"
+                  type="text"
+                  name="email"
+                  onChange={handleChange}
+                  style={{ width: "500px" }}
+                />
+              </div>
+              <div class="loginelement">
+                <TextField
+                  id="password"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  style={{ width: "500px" }}
+                />
+              </div>
+              <div class="loginelement">
+                <Button
+                  variant="contained"
+                  onClick={handleLogin}
+                  style={{ width: "500px" }}
+                >
+                  Login
+                </Button>
+              </div>
+              <div class="loginelement">
+                <Typography>
+                  Don't have an account yet?{" "}
+                  <a href="/signup" style={{ color: "blue" }}>
+                    Join here
+                  </a>
+                </Typography>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
